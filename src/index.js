9da5d39227c38c262a08a7142b19a8e77045970d
Aleks D.V. <alex.norw.jap@gmail.com>
2025-07-29 23:30:51 +0300

    feat: Solution Human Readable Number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  if (number === 0) return 'zero';
+  let num = number;
+  let result = '';
+  const hundred = 'hundred';
+  const space = ' ';
+
+  const ones = [
+    '',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+
+  const tens = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+
+  if (num >= 100) {
+    result += ones[Math.floor(num / 100)] + space + hundred;
+    num %= 100;
+    if (num > 0) {
+      result += space;
+    }
+  }
+  if (num >= 20) {
+    result += tens[Math.floor(num / 10)];
+    if (num % 10 > 0) {
+      result += space + ones[num % 10];
+    }
+  } else {
+    result += ones[num];
+  }
+
+  return result;
 };
